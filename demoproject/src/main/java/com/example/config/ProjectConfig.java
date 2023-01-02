229ebdbdbfdb0ccc6a1bee021c94f27df1238cb4
tuanpa87 <tuanpa87@users.noreply.github.com>
2023-01-02 17:36:03 +0700

    Primary Annotation

--- demoproject/src/main/java/com/example/config/ProjectConfig.java
+++ demoproject/src/main/java/com/example/config/ProjectConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import com.example.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /*
 Spring @Configuration annotation is part of the spring core framework.
@@ -33,6 +34,14 @@ public class ProjectConfig {
         return veh;
     }
 
+    /*
+      When you have multiple beans of the same kind inside the Spring context,
+      you can make one of them primary by using @Primary annotation. Primary bean is
+      the one which Spring will choose if it has multiple options and you don’t specify a name.
+      In other words, it is the default bean that Spring Context will consider in case of
+      confusion due to multiple beans present of same type.
+    * */
+    @Primary
     @Bean("ferrariVehicle")
     Vehicle vehicle3() {
         var veh = new Vehicle();
--- demoproject/src/main/java/com/example/main/Main.java
+++ demoproject/src/main/java/com/example/main/Main.java
@@ -7,15 +7,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-
-        Vehicle veh1 = context.getBean("audiVehicle",Vehicle.class);
-        System.out.println("Vehicle name from Spring Context is: " + veh1.getName());
-
-        Vehicle veh2 = context.getBean("hondaVehicle",Vehicle.class);
-        System.out.println("Vehicle name from Spring Context is: " + veh2.getName());
-
-        Vehicle veh3 = context.getBean("ferrariVehicle",Vehicle.class);
-        System.out.println("Vehicle name from Spring Context is: " + veh3.getName());
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Primary Vehicle name from Spring Context is: " + vehicle.getName());
 
     }
 }
