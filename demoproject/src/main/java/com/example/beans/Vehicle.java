4b5dbb62eb5690931ab8d98672f8cef58c1597ba
tuanpa87 <tuanpa87@users.noreply.github.com>
2023-01-02 23:19:18 +0700

    Programmatically adding beans to the Spring context

--- demoproject/src/main/java/com/example/beans/Vehicle.java
+++ demoproject/src/main/java/com/example/beans/Vehicle.java
@@ -1,9 +1,7 @@
 package com.example.beans;
 
 import org.springframework.stereotype.Component;
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-@Component
+
 public class Vehicle {
     private  String name;
 
@@ -14,18 +12,6 @@ public class Vehicle {
     public void setName(String name) {
         this.name = name;
     }
-
-    @PostConstruct
-    public void initialize() {
-        this.name = "Honda";
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println(
-                "Destroying Vehicle Bean");
-    }
-
 
     public void printHello(){
         System.out.println(
--- demoproject/src/main/java/com/example/main/Main.java
+++ demoproject/src/main/java/com/example/main/Main.java
@@ -2,15 +2,58 @@ package com.example.main;
 
 import com.example.beans.Vehicle;
 import com.example.config.ProjectConfig;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Random;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Vehicle vehicle = context.getBean(Vehicle.class);
-        System.out.println("Component Vehicle name from Spring Context is: " + vehicle.getName());
-        vehicle.printHello();
-        context.close();
+
+        Vehicle volkswagen = new Vehicle();
+        volkswagen.setName("Volkswagen");
+        Supplier<Vehicle> volkswagenSupplier = () -> volkswagen;
+
+        Supplier<Vehicle> audiSupplier = () -> {
+            Vehicle audi = new Vehicle();
+            audi.setName("Audi");
+            return audi;
+        };
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(10);
+
+        System.out.println("randomNumber = " + randomNumber);
+
+
+        if((randomNumber% 2) == 0){
+            context.registerBean("volkswagen",
+                    Vehicle.class,volkswagenSupplier);
+        }else{
+            context.registerBean("audi",
+                    Vehicle.class,audiSupplier);
+        }
+        Vehicle volksVehicle = null;
+        Vehicle audiVehicle = null;
+
+        try {
+            volksVehicle = context.getBean("volkswagen",Vehicle.class);
+        }catch (NoSuchBeanDefinitionException noSuchBeanDefinitionException){
+            System.out.println("Error while creating Volkswagen vehicle");
+        }
+        try {
+            audiVehicle = context.getBean("audi",Vehicle.class);
+        }catch (NoSuchBeanDefinitionException noSuchBeanDefinitionException){
+            System.out.println("Error while creating Audi vehicle");
+        }
+
+        if(null != volksVehicle){
+            System.out.println("Programming Vehicle name from Spring Context is: " + volksVehicle.getName());
+        }else{
+            System.out.println("Programming Vehicle name from Spring Context is: " + audiVehicle.getName());
+        }
 
     }
 }
