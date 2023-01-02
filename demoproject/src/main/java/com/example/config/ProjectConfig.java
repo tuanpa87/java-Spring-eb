a4d299eac8e2e06f59b41396689c1a870e3cb711
tuanpa87 <tuanpa87@users.noreply.github.com>
2023-01-02 23:50:49 +0700

    Wiring Beans using method parameters

--- demoproject/src/main/java/com/example/config/ProjectConfig.java
+++ demoproject/src/main/java/com/example/config/ProjectConfig.java
@@ -21,16 +21,18 @@ public class ProjectConfig {
     }
 
     /*
-    Here in the below code, we are trying to wire or establish a relationship between Person and
-    Vehicle, by invoking the vehicle() bean method from person() bean method.
-    Spring will make sure to have only 1 vehicle bean is created and also vehicle bean will
-    be created first always as person bean has dependency on it.
+      Here in the below code, we are trying to wire or establish a relationship between Person
+      and Vehicle, by passing the vehicle as a method parameter to the person() bean method.
+
+      Spring injects the vehicle bean to the person bean using Dependency Injection. Spring will
+      make sure to have only 1 vehicle bean is created and also vehicle bean will be created
+      first always as person bean has dependency on it.
     * */
     @Bean
-    public Person person() {
+    public Person person(Vehicle vehicle) {
         Person person = new Person();
         person.setName("Lucy");
-        person.setVehicle(vehicle());
+        person.setVehicle(vehicle);
         return person;
     }
 }
