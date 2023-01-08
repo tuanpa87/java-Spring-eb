735ce6ea7006594de4245b1c53f3d6a2b2e1cf6d
tuanpa87 <tuanpa87@users.noreply.github.com>
2023-01-08 23:59:23 +0700

    Prototype Bean Scope inside Spring

--- demoproject/src/main/java/com/example/main/Main.java
+++ demoproject/src/main/java/com/example/main/Main.java
@@ -8,10 +8,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
 
+
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        System.out.println("Before retrieving the Person bean from the Spring Context");
-        Person person = context.getBean(Person.class);
-        System.out.println("After retrieving the Person bean from the Spring Context");
+        VehicleServices vehicleServices1 = context.getBean(VehicleServices.class);
+        VehicleServices vehicleServices2 = context.getBean("vehicleServices",VehicleServices.class);
+        System.out.println("Hashcode of the object vehicleServices1 : " +vehicleServices1.hashCode());
+        System.out.println("Hashcode of the object vehicleServices2 : " +vehicleServices2.hashCode());
+        if(vehicleServices1==vehicleServices2){
+            System.out.println("VehicleServices bean is a singleton scoped bean");
+        }else{
+            System.out.println("VehicleServices bean is a prototype scoped bean");
+        }
     }
 }
--- demoproject/src/main/java/com/example/services/VehicleServices.java
+++ demoproject/src/main/java/com/example/services/VehicleServices.java
@@ -3,25 +3,31 @@ package com.example.services;
 import com.example.interfaces.Speakers;
 import com.example.interfaces.Tyres;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class VehicleServices {
+
     @Autowired
     private Speakers speakers;
     private Tyres tyres;
 
+    public VehicleServices(){
+        System.out.println("VehicleServices object is created");
+    }
 
-    public void playMusic() {
+    public void playMusic(){
         String music = speakers.makeSound();
         System.out.println(music);
     }
 
-    public void moveVehicle() {
+    public void moveVehicle(){
         String status = tyres.rotate();
         System.out.println(status);
     }
-
 
     public Speakers getSpeakers() {
         return speakers;
@@ -39,6 +45,4 @@ public class VehicleServices {
     public void setTyres(Tyres tyres) {
         this.tyres = tyres;
     }
-
-
 }
